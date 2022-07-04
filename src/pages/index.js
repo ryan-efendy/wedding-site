515f7e945efe5539475f7970f60d5de718095b4d
Ryan Efendy <ryanefendy95@gmail.com>
2022-07-04 14:01:13 -0700

    update home seo

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ const Image = () => {
 const IndexPage = () => {
   return (
     <>
-      <SEO title="We're getting married! And we'd love you to come to celebrate with us!" />
+      <SEO title="Omg 🙈 these kids are getting married!" />
       <div className="section">
         <div className="section-container">
           <h3 className="cursive">We're Getting Married!</h3>
