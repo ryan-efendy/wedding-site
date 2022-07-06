525abd769a7ff09e60304235c37b1d666edb8361
christyhanafi <108780388+christyhanafi@users.noreply.github.com>
2022-07-06 13:46:55 -0700

    update

--- src/pages/ourstory.js
+++ src/pages/ourstory.js
@@ -276,18 +276,15 @@ const GalleryPage = () => {
           <h3 className="cursive">Our Story</h3>
           <div className="caps-subheader accent">How We Met</div>
           <p>
-            On a Friday night in October 2019, Christy made the cross-country,
-            prime rush hour trek from Santa Monica to Arcadia after work to
-            attend the dinner portion of a leadership conference… because who
-            else would be brazen enough to skip the conference and only attend
-            dinner? This kid. She was low-key dreading it during the hour-long
-            drive, especially since her friend Stacy and sister Charissa had
-            been teasing in a group chat that many Jesus-loving male singles
-            from around the country were in attendance at this conference which
-            has proven to essentially be a real-life "Christian Mingle." Christy
-            rolled up to dinner irreverently late and sat at the saved seat
-            between Sasa and Stacy, right across the table from Ryan.
+            On a Friday night in October 2019, Christy was making the rush hour trek to Arcadia to attend the dinner portion of a leadership conference. She was low-                        key dreading it during the hour-long drive as her sister Charissa and friend Stacy had been teasing in a group chat that many Jesus-loving male singles from around the country were in attendance at this conference, which has since proven to essentially be a real-life "Christian Mingle."
             <br />
+            •••
+            Ryan had been enjoying his two days in LA as one of the members of his Seattle home church invited to be equipped during a weekend leadership conference.             He was reveling among his  friends at a very Chinese restaurant when he saw - * gasp * - a girl “making an entrance” into the noisy restaurant and walking toward him.
+            <br />
+            •••
+            Christy rolled up to dinner late and plopped down at the saved seat between Sasa and Stacy, right across the table from Ryan.
+            <br />
+            •••
             There,under the delicate lights of the ever-romantic, alluringly
             named establishment called{' '}
             <a
@@ -301,7 +298,7 @@ const GalleryPage = () => {
             of steamed fish, Ryan was smitten. As Christy began gorging herself
             on the authentic Chinese cuisine, Ryan was steadily chugging bottles
             of water and looking at Reddit for a cheesy Christian pickup line
-             to strike up a conversation with Christy.
+            to strike up a conversation.
           </p>
           {/* <Gallery
             photos={howwemetPreviewPhotos}
@@ -336,15 +333,17 @@ const GalleryPage = () => {
             Who made the first move? How?
           </div>
           <p>
-            Though nothing eventful happened the night (or weekend) we met,
-            Ryan went into research mode. He found Christy’s Instagram a few
-            days later through another conference attendant he befriended named 
+            Though nothing eventful happened when we met,
+            Ryan proceeded into research mode. He found Christy’s Instagram a few
+            days later through another conference attendant he befriended named
             Josh (he’s resourceful like that). He slid into the DMs like
-            only a classic, chivalrous Gen-Z could. Christy saw the message
-            flash across her phone and began to mildly panic. Why was this guy
-            messaging her? Why does he use so many emojis? Is it because he… God
-            forbid… is single and ready to mingle? It was too much for her, and
-            she quickly alerted Sasa and Stacy that one of those Jesus-loving
+            only a classic, chivalrous Gen-Z could.
+            <br />
+            Christy saw the message flash across her phone and began to mildly panic. Why would this guy
+            message her? Why does he use so many emojis? Is it because he… God
+            forbid… is single and ready to mingle?
+            <br />
+            It was too much for her, and she quickly alerted Sasa and Stacy that one of those Jesus-loving
             males dared to start a conversation. She breathed a sigh of relief
             when she opened his message as he seemingly innocently was inquiring
             about a small group (which Christy leads) for his “friend” that was
@@ -365,11 +364,11 @@ const GalleryPage = () => {
             in November. On this very first date, Ryan dared to admit out loud
             to Christy that the Lord spoke to him and led him to believe that
             Christy was the one he had been praying for. Naturally, Christy
-            thought he was a psychopath. She pushed him away until it was
-            undeniable that he was legit and was astounded to see that Ryan
-            would remain as serious as ever about her and their relationship as
-            he was in the beginning. He demonstrated this by leaving his
-            comforting community in Seattle to move to Christy's turf in LA and
+            thought he was cuckoo for cocoa puffs.
+            <br />
+            She countered his confidence with hesitance until it was undeniable that Ryan was the real deal, and would remain as serious and committed as ever about her and their relationship as he was in the beginning.
+            <br />
+            He demonstrated this by leaving his comforting community in Seattle to move to Christy's turf in LA and
             join her in the ministry she told him about when they first met.
             Since then, they've been serving together and growing together in
             Christ.
